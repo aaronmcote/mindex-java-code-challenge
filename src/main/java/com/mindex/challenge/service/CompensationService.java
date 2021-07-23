8e20687baeba441a3e6e93d3ba4d63e44cf5d061
Aaron Cote <aaronamc@gmail.com>
2021-07-23 12:48:07 -0400

    Added missing annotations for compensation service and controller

--- src/main/java/com/mindex/challenge/service/CompensationService.java
+++ src/main/java/com/mindex/challenge/service/CompensationService.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Service
 public class CompensationService {
     
     private static final Logger LOG = LoggerFactory.getLogger(CompensationService.class);
